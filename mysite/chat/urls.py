d853656315c149eefe680763c18ec2e452af45b3
Morkchoklad <49789750+Morkchoklad@users.noreply.github.com>
2025-01-25 22:15:56 -0500

    gotta move !!!

--- mysite/chat/urls.py
+++ mysite/chat/urls.py
@@ -1,5 +1,5 @@
 # chat/urls.py
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path("time/", views.time),
     path("game/", views.game),
     path("admin/", views.admin),
+    path("admin/qr/", views.qr),
+    path("admin/<int:severity>/<str:hospital>/", views.time),    
     path("<str:room_name>/", views.room, name="room")
 ]
--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,5 +1,7 @@
 # chat/views.py
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.http import HttpResponse
 
 
 def index(request):
@@ -8,12 +10,16 @@ def index(request):
 def game(request):
     return render(request, "chat/game.html")
 
-def time(request):
-    return render(request, "chat/time.html")
+def time(request, severity, hospital):
+    return render(request, "chat/time.html", {"hospital": hospital, "severity": severity})
 
 def admin(request):
     return render(request, "chat/admin.html")
 
+@csrf_exempt
+def qr(request):
+    return render(request, "chat/qr.html", {"hospital": request.POST['hospital'],"severity": request.POST['severity']})
+
+
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})  
-
